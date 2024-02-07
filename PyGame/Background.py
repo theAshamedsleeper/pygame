bb1dcf286d933fb506dc3cacb1ab222088bd0f45
BjarkeAR <91613897+BjarkeAR@users.noreply.github.com>
2024-02-07 12:43:23 +0100

    yo
    yo

--- PyGame/Background.py
+++ PyGame/Background.py
@@ -1,12 +1,25 @@
 import pygame
 from Components import Component
+from GameObject import GameObject
 
+#instanciation method
+      
 class Background(Component):
-    def __init__(self, image_path, scroll_speed) -> None:
+    def __init__(self, game_world, image_path, scroll_speed) -> None:
         super().__init__()
-        self._background_image = pygame.image.load(image_path)
+        self._game_world = game_world
+        self._background_image = pygame.image.load(f"Assets\\ArcticAssets\\RunningBackgrounds\\{image_path}")
         self._scroll_speed = scroll_speed
         self._x_position = 0
+
+        # Scale the background image to fit the screen size
+        screen_width = self._game_world.screen.get_width()
+        screen_height = self._game_world.screen.get_height()
+        scaled_width = screen_width * 4
+        self._background_image = pygame.transform.scale(self._background_image, (scaled_width, screen_height))
+
+    def awake(self, game_world):
+        return super().awake(game_world)
 
     def start(self):
         pass
