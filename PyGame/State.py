f8f074097bdae722492fba4969e8805f789bb3f2
BjarkeAR <91613897+BjarkeAR@users.noreply.github.com>
2024-02-13 11:58:57 +0100

    yo
    yo

--- PyGame/State.py
+++ PyGame/State.py
@@ -195,9 +195,20 @@ class FirstLevelState(State):
         super().__init__(game_world)
         
         self._background_image_path ="SimpleBackgroundClear.png"
-        self._scroll_speed = 300
+        self._scroll_speed = 50
         self._background_go = GameObject(position=(0, 0))
         self._background_go.add_component(Background(game_world, image_path=self._background_image_path, scroll_speed=self._scroll_speed))
+
+        self._middle_ground_image_path = "GravelTrans.png"
+        self._middle_ground_scroll_speed = 100
+        self._middle_ground_go = GameObject(position=(0, 0))
+        self._middle_ground_go.add_component(Background(game_world, image_path=self._middle_ground_image_path, scroll_speed=self._middle_ground_scroll_speed))
+
+        self._fore_ground_image_path = "SandTrans.png"
+        self._fore_ground_scroll_speed = 150
+        self._fore_ground_go = GameObject(position=(0, 0))
+        self._fore_ground_go.add_component(Background(game_world, image_path=self._fore_ground_image_path, scroll_speed=self._fore_ground_scroll_speed))
+
 
         go_mothership = GameObject(pygame.math.Vector2(0,0))
         go_mothership.add_component(SpriteRenderer("space_breaker_asset\\Others\\Stations\\station.png"))
@@ -259,6 +270,10 @@ class FirstLevelState(State):
         self._game_world.screen.fill("lightcoral")
 
         self._background_go.update(delta_time)
+        self._middle_ground_go.update(delta_time)
+        self._fore_ground_go.update(delta_time)
+        
+        
 
         #Makes a copy om _gameObjects and runs through that instead of the orginal
         for gamObjects in self._gameObjects[:]:
