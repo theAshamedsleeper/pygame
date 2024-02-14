82b130021c8ffb31f283c72a21fd5963c3405eff
BjarkeAR <91613897+BjarkeAR@users.noreply.github.com>
2024-02-14 13:02:46 +0100

    yo
    yo

--- PyGame/GameWorld.py
+++ PyGame/GameWorld.py
@@ -1,6 +1,8 @@
 import pygame
 from State import MenuState
 from State import FirstLevelState
+from State import SecondLevelState
+from State import ThirdLevelState
 from State import loosOrVicState
 from GameObject import GameObject
 
@@ -14,7 +16,7 @@ class GameWorld:
         self._running = True
         self._clock = pygame.time.Clock()
         self._clock.tick(60)
-        menu = MenuState(self)
+        menu = ThirdLevelState(self)
         self._currentState = menu
         self._nextState = None
         self._newState = None
