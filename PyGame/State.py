8e75e437fad5cd08386cb9e7fedcf14e4858bd46
Nicolai-Jensen <nicolaijensen11@gmail.com>
2024-02-15 11:58:57 +0100

    Hello darkness my old friend

--- PyGame/State.py
+++ PyGame/State.py
@@ -226,6 +226,7 @@ class FirstLevelState(State):
         mixer.music.play(-1)
         mixer.music.set_volume(.03)
 
+
         go_mothership = GameObject(pygame.math.Vector2(0,0))
         go_mothership.add_component(SpriteRenderer("space_breaker_asset\\Others\\Stations\\station.png"))
         go_mothership.add_component(MotherShip())
@@ -250,7 +251,7 @@ class FirstLevelState(State):
 
 
         go_player = GameObject(pygame.math.Vector2(0,0))
-        go_player.add_component(SpriteRenderer("player_ship.png"))
+        go_player.add_component(SpriteRenderer("space_breaker_asset\\Ships\\Small\\body_01.png"))
         go_player.add_component(Player())
         
         
