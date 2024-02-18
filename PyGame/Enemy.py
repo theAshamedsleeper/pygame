3c4ef09c67ed0f0e2eccb8881acf9ef9dcfe8d1c
Kasp040g <kasperlaks@gmail.com>
2024-02-18 23:49:37 +0100

    de gpår op og ned

--- PyGame/Enemy.py
+++ PyGame/Enemy.py
@@ -9,7 +9,7 @@ class Enemy(Component):
     def __init__(self, scale_factor=0.3) -> None:
         super().__init__()
         self.speed_x = 200
-        self.speed_y = 0
+        self.speed_y = 50
         self.scale_factor = scale_factor
         self.stop_x_position = 1100 #X position where the enemies stop
         self.direction = 1  # Initial direction: 1 for down, -1 for up
@@ -33,18 +33,18 @@ class Enemy(Component):
         pass
 
     def update(self, delta_time):
-        self._gameObject.transform.position.x -= self.speed_x * delta_time
-        self._gameObject.transform.position.y -= self.speed_y * delta_time * self.direction
 
         if self._gameObject.transform.position.x <= self.stop_x_position:
-            self.speed_x = 0
-            self.speed_y = 50
-            
+             self._gameObject.transform.position.x -= self.speed_x * delta_time
 
-        if  self._gameObject.transform.position.y <= 0:  # Reached top of the screen
-                self.speed_y = -50  # Change direction to down
-        elif self._gameObject.transform.position.y >= self._screen_size.y:  # Reached bottom of the screen
+        if self.direction == 1:
+            self._gameObject.transform.position.y += self.speed_y * delta_time
+            if self._gameObject.transform.position.y >= self._screen_size.y:
                 self.direction = -1  # Change direction to up
+        else:
+            self._gameObject.transform.position.y -= self.speed_y * delta_time
+            if self._gameObject.transform.position.y <= 0:
+                self.direction = 1  # Change direction to down
             
 
             
