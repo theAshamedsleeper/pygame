26e8a3401fa0777ae789d39ec6e5eaf107e4c270
Kasp040g <kasperlaks@gmail.com>
2024-02-19 12:35:19 +0100

    da

--- PyGame/Enemy.py
+++ PyGame/Enemy.py
@@ -13,9 +13,7 @@ class Enemy(Component):
         self.scale_factor = scale_factor
         self.stop_x_position = 1100 #X position where the enemies stop
         self.direction = 1  # Initial direction: 1 for down, -1 for up
-        
-
-
+    
     def awake(self, game_world):
         self._game_world = game_world
         sr = self._gameObject.get_component("SpriteRenderer")
@@ -27,9 +25,6 @@ class Enemy(Component):
         min_y = 80  # Minimum y-coordinate for enemy spawn
         max_y = max(min_y, self._screen_size.y)  # Ensure max_y is at least min_y
         self._gameObject.transform.position.y = random.randint(180, int(self._screen_size.y)) + (self._sprite_size.y)
-        #self._gameObject.transform.position.y = (self._screen_size.y) + (self._sprite_size.y)  
-
-
 
     def start(self):
         pass
@@ -40,21 +35,13 @@ class Enemy(Component):
 
         if self._gameObject.transform.position.x <= self.stop_x_position:
             self.speed_x = 0
-        # if    self._gameObject.transform.position.x <= self.stop_x_position-100 and self.speed_x==200:
-        #     self.speed_x = 0
-        # if    self._gameObject.transform.position.x <= self.stop_x_position-200 and self.speed_x==199:
-        #     self.speed_x = 0
-        # if    self._gameObject.transform.position.x <= self.stop_x_position-300 and self.speed_x>=51:
-        #     self.speed_x = 0
-
-
+        
         if self.direction == 1:
             self._gameObject.transform.position.y += self.speed_y * delta_time
             if self._gameObject.transform.position.y >= 720:
                 self.direction = -1  # Change direction to up
                 self.stop_x_position-=100
                 self.speed_x = 200
-
         else:
             self._gameObject.transform.position.y -= self.speed_y * delta_time
             if self._gameObject.transform.position.y <= 180:
