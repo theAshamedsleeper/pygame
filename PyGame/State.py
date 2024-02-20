fb9bb9c2973de3ea67c6d4b5dc240510572768ed
Kasp040g <kasperlaks@gmail.com>
2024-02-20 10:40:31 +0100

    kaspbknåd

--- PyGame/State.py
+++ PyGame/State.py
@@ -230,7 +230,6 @@ class FirstLevelState(State):
     def __init__(self, game_world) -> None:
         super().__init__(game_world)
         self.clock = pygame.time.Clock()
-        #self.enemy_spawner = EnemySpawner(game_world)
 
 
         self._player_score = 0
@@ -258,6 +257,8 @@ class FirstLevelState(State):
         self._effect_ground_go = GameObject(position=(0, 0))
         self._effect_ground_go.add_component(Background(game_world, image_path=self._effect_ground_image_path, scroll_speed=self._effect_ground_scroll_speed))
 
+        self.enemy_delay = 3
+        self.enemy_timer = 0
 
         # background_music = mixer
         self._music = mixer.music.load("Assets\\Audio\\Background.mp3")
@@ -303,6 +304,14 @@ class FirstLevelState(State):
         img = font.render(text, True, text_col)
         self._game_world.screen.blit(img,(x,y))
 
+    def spawn_enemy(self):
+        go_enemy = GameObject(pygame.math.Vector2(0,0))
+        go_enemy.add_component(SpriteRenderer("ship_1782.png"))
+        go_enemy.add_component(Enemy())
+
+        self.instantiate(go_enemy)
+        print("enemy spawned")
+
     def instantiate(self, gameObject):
         gameObject.awake(self._game_world)
         gameObject.start()
@@ -339,10 +348,17 @@ class FirstLevelState(State):
     def update(self, delta_time):
         # fill the screen with a color to wipe away anything from last frame
         self._game_world.screen.fill("lightcoral")
+        self.enemy_timer +=delta_time
+
 
         self._background_go.update(delta_time)
         self._fore_ground_go.update(delta_time)
         self._middle_ground_go.update(delta_time)
+
+        if self.enemy_timer >= self.enemy_delay:
+            self.spawn_enemy()
+            self.enemy_timer = 0 #resets cooldown after shoot()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -464,9 +480,6 @@ class SecondLevelState(State):
     def spawn_enemy(self):
         go_enemy = GameObject(pygame.math.Vector2(0,0))
         go_enemy.add_component(SpriteRenderer("ship_1782.png"))
-      #  sprite_renderer = SpriteRenderer(sprite_name="ship_178.png")
-      #  sprite_renderer.scale(0.1)
-       # go_enemy.add_component(sprite_renderer)
         go_enemy.add_component(Enemy())
 
         self.instantiate(go_enemy)
@@ -557,7 +570,9 @@ class ThirdLevelState(State): #Boss level
         self._effect_groundv3_scroll_speed = 2500
         self._effect_groundv3_go = GameObject(position=(0, 0))
         self._effect_groundv3_go.add_component(Background(game_world, image_path=self._effect_groundv3_image_path, scroll_speed=self._effect_groundv3_scroll_speed))
-
+        
+        self.enemy_delay = 3
+        self.enemy_timer = 0    
 
         # background_music = mixer
         mixer.music.load("Assets\\Audio\\Background.mp3")
@@ -603,6 +618,14 @@ class ThirdLevelState(State): #Boss level
         self._gameObjects.append(go_turret_three)
         self._gameObjects.append(go_turret_four)
 
+    def spawn_enemy(self):
+        go_enemy = GameObject(pygame.math.Vector2(0,0))
+        go_enemy.add_component(SpriteRenderer("ship_1782.png"))
+        go_enemy.add_component(Enemy())
+
+        self.instantiate(go_enemy)
+        print("enemy spawned")
+
 
 
     def instantiate(self, gameObject):
@@ -626,6 +649,8 @@ class ThirdLevelState(State): #Boss level
         self._game_world.screen.fill("lightcoral")
         self._backgroundv3_go.update(delta_time)
         self._middle_groundV3_go.update(delta_time)
+        self.enemy_timer +=delta_time
+
         
         self.fps_counter(self.clock, self._game_world.screen)
         delta_time = self.clock.tick(60) / 1000.0 # limits FPS to 60
@@ -637,6 +662,10 @@ class ThirdLevelState(State): #Boss level
         self._gameObjects = [obj for obj in self._gameObjects if not obj._is_destroyed]
         self._fore_groundV3_go.update(delta_time)
         self._effect_groundv3_go.update(delta_time)
+
+        if self.enemy_timer >= self.enemy_delay:
+            self.spawn_enemy()
+            self.enemy_timer = 0 #resets cooldown after shoot()
 
     def makeTurret(self, string):
         turret = GameObject(pygame.math.Vector2(0,0))
